65beab16c6f95ae1ff3efdec7fb6c3542f752788
FadiSheh <58226655+FadiSheh@users.noreply.github.com>
2020-11-15 13:51:42 +0100

    Serveur TCP

---
+++ clientTCP.c
@@ -0,0 +1,242 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+
+//FONCTIONS MENU ET ACTIONS POSSIBLES
+int menu(){
+
+	int choix;
+    printf("\n");
+    printf("Gestion du compte : choix de l'action\n");
+    printf("[1] Ajout d'une somme \n");
+    printf("[2] Retrait d'une somme \n");
+    printf("[3] Afficher solde\n");   
+    printf("[4] Afficher 10 dernières opérations\n");
+    printf("[5] Déconnexion\n");
+    printf("\n");
+
+    scanf("%d", &choix);
+    return choix;
+}
+
+void ajoutSomme(int* nbCompte, float* somme){
+		//on demande le compte puis la somme
+	printf("Veuillez saisir le numéro de compte\n");
+	scanf("%d", nbCompte);
+	printf("Veuillez indiquer la somme à ajouter\n");
+	scanf("%f", somme);
+}
+
+void retraitMontant(int* nbCompte, float* somme){
+	printf("Veuillez saisir le numéro de compte\n");
+	scanf("%d", nbCompte);
+	printf("Veuillez indiquer la somme à retirer\n");
+	scanf("%f", somme);
+}
+
+void recupNbCompte(int* nbCompte){
+	printf("Veuillez saisir le numéro de compte\n");
+	scanf("%d", nbCompte);
+}
+
+
+
+//MAIN
+int main(int argc, char *argv[]){
+
+	//declaration des variables
+    int sockfd, portno, m, n;
+   	int flag;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    int nbCompte;
+    float somme;
+    char buffer[512];
+    char receiveBuffer[512];
+
+    //si nombre de variables invalide
+	if (argc<4)
+	{
+		fprintf(stderr,"./client <server_address> <server_numport> <message>\n");
+		fprintf(stderr,"ex: ./client localhost 32000 \"joe\"\n");
+		return 1;
+	}
+
+	//recuperation du numero du port
+    portno = atoi(argv[2]);
+
+	//creation de la socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) 
+	{
+		fprintf(stderr,"Impossible d'ouvrir la socket. On arrête tout.\n");
+		return 1;
+	}
+    
+    //recuperation du numero du serveur
+	server = gethostbyname(argv[1]);
+    if (server == NULL) 
+	{
+        	fprintf(stderr,"Impossible de récupérer les infos sur le serveur\n");
+		return 1;
+    }
+
+	// On donne toutes les infos sur le serveur
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+	//connexion de la socket
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+	{
+		fprintf(stderr,"Impossible de faire l'appel system connect().\n");
+		return 1;
+	}
+
+
+	//connexion : 0 est le premier element, id du client, mdp du client (1ere trame envoyée) (c'est quoi le 1?)
+	//ETAPE DE CONNEXION
+	char identifiant[10];
+	char mdp[15];
+
+	
+
+	printf("Connexion au server\nBonjour %s\n", argv[3]);
+	printf("Veuillez renseigner votre identifiant :\n");
+    scanf("%s", identifiant);
+	printf("Veuillez renseigner votre mdp:\n");
+	scanf("%s", mdp);
+	//printf("%s\n", mdp);
+	
+	//on stocke ces données dans un buffer
+	sprintf(buffer, "%d %s %s", 0, identifiant, mdp);
+    n = write(sockfd,buffer,strlen(buffer));
+
+    printf("Données de connexion envoyées \n");
+    printf("Attente de la réponse du server...\n");
+
+
+	n = read(sockfd,receiveBuffer,512);
+
+	close(sockfd);
+
+
+	char verif_identifiants[15];
+
+
+	printf("%s\n\n",receiveBuffer);
+
+	sscanf(receiveBuffer,"%s",verif_identifiants);
+	//demander l'action si la reponse du serveur est valide
+	if(strcmp("OK",verif_identifiants) == 0){
+
+		printf("Connexion Réussie\n");
+		//si la premiere donnée est un 1 => on peut passer dans le menu
+		m = menu();
+		flag = 1;
+	} else { printf("Identifiants Erronés\n");exit(0);}
+
+	//en fonction de laction choisie on demande infos supp
+	//FAIRE DES FONCTIONS
+	while(flag){
+		if(m == 1){
+			//ajout d'une somme
+			ajoutSomme(&nbCompte, &somme);
+			//on change l'état du buffer
+			//id_client  id_compte  password  somme
+			sprintf(buffer, "%d %s %d %s %f", m, identifiant, nbCompte, mdp, somme);
+			flag = 1;
+		}
+		else if(m == 2){
+			//Retrait d'une somme
+			//on demande le compte puis la somme
+			retraitMontant(&nbCompte, &somme);
+			//on change l'état du buffer
+			//<id_client id_compte password somme>
+			sprintf(buffer, "%d %s %d %s %f", m, identifiant, nbCompte, mdp, somme);
+			flag = 1;			
+		}
+		else if (m == 3){
+			//Afficher solde
+			recupNbCompte(&nbCompte);
+			//on change l'état du buffer
+			//id_client id_compte password
+			sprintf(buffer, "%d %s %d %s", m, identifiant, nbCompte, mdp);
+			flag = 1;			
+		}
+		else if(m == 4){
+			//Afficher 10 dernières opérations
+			recupNbCompte(&nbCompte);
+			//on change l'état du buffer
+			//id_client id_compte password
+			sprintf(buffer, "%d %s %d %s", m, identifiant, nbCompte, mdp);
+			flag = 1;
+		}
+		else if (m == 5){
+			//Déconnexion
+			printf("Déconnexion en cours...\n");
+			close(sockfd);
+			flag = 0;
+			return 1;
+		}
+		
+		else if (m>5){
+			printf("!! Veuillez choisir une action valide !!\n");
+			
+			flag = 1;	
+		}
+
+   
+
+	//creation de la socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) 
+	{
+		fprintf(stderr,"Impossible d'ouvrir la socket. On arrête tout.\n");
+		return 1;
+	}
+
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+	{
+		fprintf(stderr,"Impossible de faire l'appel system connect().\n");
+		return 1;
+	}
+
+
+    n = write(sockfd,buffer,strlen(buffer));
+    printf("\nMessage envoyé\nAttente de la réponse\n");
+
+
+    if(m==4){sleep(1);}
+
+
+    n = read(sockfd,receiveBuffer,512);
+    
+   	printf("\n%s", receiveBuffer);
+	
+	// On ferme la socket
+    //printf("Déconnexion\n");
+    close(sockfd);
+    bzero(receiveBuffer,512);  
+
+    printf("\n");   
+    m = menu();
+
+
+
+
+	}
+
+
+
+    return 0;
+}
---
+++ serverTCP.c
@@ -0,0 +1,322 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+// Données sur les clients
+//time_t rawtime = time(NULL);
+
+
+typedef struct{
+
+    float montant;
+    int type_operation;    //0 AJOUT 1 RETRAIT
+    time_t date;
+
+} case_tableau ;
+
+
+
+struct _client {
+
+    char identifiant[10];
+    char mdp[15];
+
+    case_tableau compte[2][11];
+
+} clients[2];
+
+
+
+int verif_identifiants(char* identifiant, char* mdp){
+
+
+  for (int i = 0 ; i<2 ; i++){
+
+    if ( strcmp(clients[i].identifiant,identifiant)==0){
+
+        if (strcmp(clients[i].mdp,mdp) == 0){  return i;} 
+            else {return -1;}
+}}
+
+return -1;}
+
+
+
+
+void deplacer(int nbCompte,int indice_client){
+  
+    for (int i = 10 ;i>1;i--){
+
+    clients[indice_client].compte[nbCompte][i]=clients[indice_client].compte[nbCompte][i-1];
+   
+
+    }
+}
+
+
+
+
+void ajout_montant(float somme,int nbCompte,int indice_client){
+
+
+    deplacer(nbCompte,indice_client);
+
+    clients[indice_client].compte[nbCompte][0].montant = clients[indice_client].compte[nbCompte][0].montant+somme;
+    clients[indice_client].compte[nbCompte][0].type_operation = 0;
+    clients[indice_client].compte[nbCompte][0].date = time(NULL);
+
+
+    clients[indice_client].compte[nbCompte][1]=clients[indice_client].compte[nbCompte][0];
+    clients[indice_client].compte[nbCompte][1].montant = somme;
+
+
+    printf("Solde du client : %.f\n",clients[indice_client].compte[nbCompte][0].montant);
+    printf("Type operation : %d\n",clients[indice_client].compte[nbCompte][0].type_operation);
+   
+    struct tm *ptm = localtime(&clients[indice_client].compte[nbCompte][0].date);
+
+    printf("date: %02d/%02d/%02d\n",ptm->tm_mday,ptm->tm_mon,1900+ptm->tm_year);
+
+}
+
+
+
+
+void retrait_montant(float somme,int nbCompte,int indice_client){
+deplacer(nbCompte,indice_client);
+
+    clients[indice_client].compte[nbCompte][0].montant = clients[indice_client].compte[nbCompte][0].montant-somme;
+    clients[indice_client].compte[nbCompte][0].type_operation = 1;
+    clients[indice_client].compte[nbCompte][0].date = time(NULL);
+
+    clients[indice_client].compte[nbCompte][1]=clients[indice_client].compte[nbCompte][0];
+    clients[indice_client].compte[nbCompte][1].montant = somme;
+
+     printf("Solde du client : %.f\n",clients[indice_client].compte[nbCompte][0].montant);
+    printf("Type operation : %d\n",clients[indice_client].compte[nbCompte][0].type_operation);
+   
+    struct tm *ptm = localtime(&clients[indice_client].compte[nbCompte][0].date);
+
+    printf("date: %02d/%02d/%02d\n",ptm->tm_mday,ptm->tm_mon,1900+ptm->tm_year);
+
+}
+
+
+case_tableau afficher_solde(int nbCompte,int indice_client){
+
+return clients[indice_client].compte[nbCompte][0];
+
+}
+
+
+
+char * historique(int indice_client,int nbCompte,int indice){
+char *buffer = malloc (sizeof (char) * 512);
+bzero(buffer,512);
+char jma[11];
+char fonction[10];
+
+    case_tableau hist;
+
+     hist = clients[indice_client].compte[nbCompte][indice];
+
+     struct tm *ptm = localtime(&hist.date);
+     sprintf(jma,"%02d/%02d/%02d",ptm->tm_mday,ptm->tm_mon,1900+ptm->tm_year);
+
+     if (hist.type_operation == 0){ sprintf(fonction,"%s","AJOUT"); }else {sprintf(fonction,"%s","RETRAIT");}
+
+            sprintf(buffer,"%s %s %s %.f\n","RES_OPERATIONS",fonction,jma, hist.montant);
+            
+            return buffer;
+
+    
+
+}
+int main(int argc, char *argv[]){   
+
+//Init client 
+sprintf(clients[0].identifiant,"123");
+//clients[0].identifiant = "123";
+sprintf(clients[0].mdp,"321");
+//clients[0].mdp = "321";
+clients[0].compte[0][0].montant = 500;
+
+for (int i = 1; i<11 ;i++){
+    clients[0].compte[0][i].montant = 0;
+    clients[0].compte[0][i].type_operation = 0;
+    clients[0].compte[0][i].date = time(NULL);
+}
+
+sprintf(clients[1].identifiant,"456");
+//clients[0].identifiant = "123";
+sprintf(clients[1].mdp,"654");
+//clients[0].mdp = "321";
+clients[1].compte[0][0].montant = 500;
+
+for (int i = 1; i<11 ;i++){
+    clients[1].compte[0][i].montant = 0;
+    clients[1].compte[0][i].type_operation = 0;
+    clients[1].compte[0][i].date = time(NULL);
+}
+    
+    //printf("Identifiant: %s\nMot de passe: %s\nMontant: %f\n ",clients[0].identifiant,clients[0].mdp,clients[0].compte_1[0].montant);
+    //Initialisation des différentes variables
+
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[512];
+    char sendBuffer[512];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n,res_bind;
+    case_tableau Val;
+
+    // Création de la socket TCP
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1){ printf("Erreur lors de l'ouverture de la socket\n"); return 1;}
+
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi("32000");                                 //Le port par défaut pour le serveur est le port 32000
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;                 //Socket affectée à toutes les interfaces
+    serv_addr.sin_port = htons(portno);
+
+
+
+    res_bind = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    if ( res_bind< 0){ printf("Erreur lors de l'appel système bind()\n"); return 1;}
+
+
+
+    listen(sockfd,5);                                      //6 connexions pendantes maximum
+ 
+    while (1){
+
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+        if (newsockfd < 0){ printf("Erreur lors de l'appel système accept()\n"); return 1;}
+
+
+        bzero(buffer,512);                                //(Re)initialisation de buffer
+        n = read(newsockfd,buffer,255);
+
+        if (n < 0){ printf("Erreur lors de l'appel système read()\n"); return 1;}
+
+
+        //printf("Message reçu par %s:%d\nData: %s\n\n",inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port), buffer);
+
+
+        //CONNEXION
+        char identifiant[10];
+        char mdp[15];
+        int action;
+        int ok=0;
+        int nbCompte;
+        float somme;
+        int indice_client;
+        float solde;
+        char jma[11];
+
+        sscanf(buffer,"%d",&action);
+        bzero(sendBuffer,512); 
+
+        printf("\n");
+        printf("Message reçu part %s:%d\n\n",inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+
+
+        if (action==0){
+
+            printf("Le client se connecte\n");
+            sscanf(buffer,"%d %s %s",&action,identifiant,mdp);
+            printf("Action: %d   \nidentifiant: %s  \nMot de passe: %s \n",action,identifiant,mdp);
+            indice_client= verif_identifiants(identifiant,mdp);
+            printf("indice %d\n",indice_client);
+            if (indice_client != -1){sprintf(sendBuffer,"OK\n");
+            printf("Connexion acceptée\n");} else {sprintf(sendBuffer,"KO\n");printf("Connexion échouée\n");}
+            n=write(newsockfd,sendBuffer,strlen(sendBuffer));
+
+
+        }
+
+
+        if (action==1){ 
+
+            printf("Le client fait un ajout\n");
+            sscanf(buffer,"%d %s %d %s %f",&action,identifiant,&nbCompte, mdp, &somme);
+            indice_client= verif_identifiants(identifiant,mdp);
+            printf("Action: %d\nIdentifiant: %s\nNcompte:%d\nMot de passe: %s\nSomme: %f \n",action,identifiant,nbCompte, mdp, somme); 
+            if( (nbCompte>2) || (nbCompte<0)){ok = 0;} else { ok =1; ajout_montant(somme,nbCompte,indice_client);}
+            if(ok ==1) {sprintf(sendBuffer,"OK\n");printf("Ajout Réussi\n");} else { sprintf(sendBuffer,"KO\n"); printf("Ajout Echoué\n");}
+            n=write(newsockfd,sendBuffer,strlen(sendBuffer)); 
+            
+
+        }
+
+        if (action==2){ 
+
+            printf("Le client fait un retrait\n");
+            sscanf(buffer,"%d %s %d %s %f",&action,identifiant,&nbCompte, mdp, &somme);
+            indice_client= verif_identifiants(identifiant,mdp);
+            printf("Action: %d\nIdentifiant: %s\nNcompte:%d\nMot de passe: %s\nSomme: %f \n",action,identifiant,nbCompte, mdp, somme);
+            if( (nbCompte>2) || (nbCompte<0)){ok = 0;} else { ok =1;}
+            retrait_montant(somme,nbCompte,indice_client);
+            if(ok ==1) {sprintf(sendBuffer,"OK\n");printf("Retrait Réussi\n");} else { sprintf(sendBuffer,"KO\n"); printf("Retrait Echoué\n");}
+            n=write(newsockfd,sendBuffer,strlen(sendBuffer));  
+            
+
+        }
+      
+
+        if (action==3){ 
+
+            printf("Le client veut afficher son solde\n");
+            sscanf(buffer,"%d %s %d %s",&action,identifiant,&nbCompte, mdp);
+            indice_client= verif_identifiants(identifiant,mdp);
+            printf("Action: %d\nIdentifiant: %s\nNcompte:%d\nMot de passe: %s\n",action,identifiant,nbCompte, mdp);
+            if( (nbCompte>2) || (nbCompte<0)){ok = 0;} else { ok =1; Val = afficher_solde(nbCompte,indice_client);}
+
+            struct tm *ptm = localtime(&Val.date);
+            sprintf(jma,"%02d/%02d/%02d",ptm->tm_mday,ptm->tm_mon,1900+ptm->tm_year);
+
+            sprintf(sendBuffer,"%s %.f %s","RES_SOLDE",Val.montant,jma);
+            n=write(newsockfd,sendBuffer,strlen(sendBuffer)); 
+        }
+
+        if (action==4){ 
+
+            printf("Le client veut afficher son historique\n");
+            sscanf(buffer,"%d %s %d %s",&action,identifiant,&nbCompte, mdp);
+            indice_client= verif_identifiants(identifiant,mdp);
+            printf("Action: %d\nIdentifiant: %s\nNcompte:%d\nMot de passe: %s\n",action,identifiant,nbCompte, mdp);
+
+            for(int i = 1; i<11;i++){
+
+                printf("%s \n",historique(indice_client,nbCompte,i));
+
+            }
+
+            for(int i = 1; i<11;i++){
+            bzero(sendBuffer,512); 
+            sprintf(sendBuffer,"%s",historique(indice_client,nbCompte,i));
+            n=write(newsockfd,sendBuffer,strlen(sendBuffer));  }
+            
+        }
+
+   
+        
+        bzero(buffer,512);      
+        close(newsockfd);
+        
+        }
+        
+        close(sockfd);
+        return 0; 
+}
